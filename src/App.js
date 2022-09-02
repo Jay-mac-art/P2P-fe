73777c2e3512cc30a27b61536cc8d955c4ebec56
Jaymac01 <jayantkumar@validatemeinc.com>
2022-09-02 11:42:55 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+ 
+  useEffect(  () => {
+   axios
+      .get("http://localhost:3000/get/testimonials")
+      .then((res) => { setData(res.data)})
+      .catch((err) => console.log(err, "it has an error"));
+     
+     
+    
+    } ,[]);
+    
   return (
     <div >
       <head>
@@ -286,60 +300,51 @@ function App() {
                 </div>
               </div>
             </div>
-            <div className="row">
+            
+
+           
+                         
+            
+             <div className="row">
               <div className="col-lg-12">
                 <div className="row testimonial-active">
-                  <div className="col-lg-4">
+
+                {data  && data.map((singleData) => {
+
+                 return  <div className="col-lg-4"> 
+                  
                     <div className="single-testimonial mt-30 mb-30 text-center">
                       <div className="testimonial-image">
-                        <img src="assets/images/author-3.jpg" alt="Author" />
+
+                      
+                        <img src = 
+                        {`uploads/${singleData.photo}`} 
+                        alt="Author"
+                         />
+                     
                       </div>
+
+                      
                       <div className="testimonial-content">
-                        <p className="text">Stop wasting time and money designing and managing a website that doesn’t get results. Happiness guaranteed! Stop wasting time and money designing and managing a website that doesn’t get results. Happiness guaranteed!</p>
-                        <h6 className="author-name">Isabela Moreira</h6>
-                        <span className="sub-title">CEO, GrayGrids</span>
+
+
+                        <p className="text">{singleData.testimonial_Description}</p>
+                        <h6 className="author-name">{singleData.name}</h6>
+                        <span className="sub-title">{singleData.post}</span>
+                      
                       </div>
+                       
+                     
                     </div>
+                    
                   </div>
-                  <div className="col-lg-4">
-                    <div className="single-testimonial mt-30 mb-30 text-center">
-                      <div className="testimonial-image">
-                        <img src="assets/images/author-1.jpg" alt="Author" />
-                      </div>
-                      <div className="testimonial-content">
-                        <p className="text">Stop wasting time and money designing and managing a website that doesn’t get results. Happiness guaranteed! Stop wasting time and money designing and managing a website that doesn’t get results. Happiness guaranteed!</p>
-                        <h6 className="author-name">Fiona</h6>
-                        <span className="sub-title">Lead Designer, UIdeck</span>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-lg-4">
-                    <div className="single-testimonial mt-30 mb-30 text-center">
-                      <div className="testimonial-image">
-                        <img src="assets/images/author-2.jpg" alt="Author" />
-                      </div>
-                      <div className="testimonial-content">
-                        <p className="text">Stop wasting time and money designing and managing a website that doesn’t get results. Happiness guaranteed! Stop wasting time and money designing and managing a website that doesn’t get results. Happiness guaranteed!</p>
-                        <h6 className="author-name">Elon Musk</h6>
-                        <span className="sub-title">CEO, SpaceX</span>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-lg-4">
-                    <div className="single-testimonial mt-30 mb-30 text-center">
-                      <div className="testimonial-image">
-                        <img src="assets/images/author-4.jpg" alt="Author" />
-                      </div>
-                      <div className="testimonial-content">
-                        <p className="text">Stop wasting time and money designing and managing a website that doesn’t get results. Happiness guaranteed! Stop wasting time and money designing and managing a website that doesn’t get results. Happiness guaranteed!</p>
-                        <h6 className="author-name">Fajar Siddiq</h6>
-                        <span className="sub-title">CEO, MakerFlix</span>
-                      </div>
-                    </div>
-                  </div>
+                   })}
                 </div>
               </div>
-            </div>
+              
+            </div> 
+        
+
           </div>
         </section>
 
@@ -535,5 +540,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
